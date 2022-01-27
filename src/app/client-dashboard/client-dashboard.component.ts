4ae2657a0b1dce6f87f5ed7b3259e98d4c5e35a3
Moses <moses.junior@student.moringaschool.com>
2022-01-27 15:40:14 +0300

    temporary completion on client dashboard

--- src/app/client-dashboard/client-dashboard.component.ts
+++ src/app/client-dashboard/client-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../accountservice/accountservice.service';
 
 @Component({
   selector: 'app-client-dashboard',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-dashboard.component.css']
 })
 export class ClientDashboardComponent implements OnInit {
+  user:any
 
-  constructor() { }
+  constructor(private accountservice:AccountService) { }
 
+  logout(){
+    this.accountservice.logout()
+  }
+  
   ngOnInit(): void {
+    this.accountservice.counsellor_profile().subscribe((response:any)=>{
+      this.user = response['user']
+    })
   }
 
 }
