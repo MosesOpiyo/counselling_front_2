3d22d0ad01dc51ab3fd93bfc17b8efff9ae5bcb0
Moses <moses.junior@student.moringaschool.com>
2022-01-27 16:59:31 +0300

    implementing a groups list for the counsellor

--- src/app/counsellor-groups/counsellor-groups.component.ts
+++ src/app/counsellor-groups/counsellor-groups.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../accountservice/accountservice.service';
 
 @Component({
   selector: 'app-counsellor-groups',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CounsellorGroupsComponent implements OnInit {
 
-  constructor() { }
+  user:any
+  constructor(private accountservice:AccountService) { }
+
+  logout(){
+    this.accountservice.logout()
+  }
 
   ngOnInit(): void {
+    this.accountservice.counsellor_profile().subscribe((response:any)=>{
+      this.user = response['user']
+    })
   }
 
 }
