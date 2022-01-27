6d8c7a05ccee25305e270d9ddbcfae6632987920
Moses <moses.junior@student.moringaschool.com>
2022-01-27 15:45:36 +0300

    temporary completion on counsellor options  design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientDashboardComponent } from './client-dashboard/client-dashboard.component';
+import { CounsellorOptionsComponent } from './counsellor-options/counsellor-options.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path:'',component:SignupComponent},
   {path:'login',component:LoginComponent},
-  {path:'client_dashboard',component:ClientDashboardComponent}
+  {path:'client_dashboard',component:ClientDashboardComponent},
+  {path:'counsellor_group',component:CounsellorOptionsComponent}
 ];
 
 @NgModule({
