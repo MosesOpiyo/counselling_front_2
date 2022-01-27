0ca0a558ff351a3a576ca6be0b0badc5e62bbe99
Moses <moses.junior@student.moringaschool.com>
2022-01-27 15:37:44 +0300

    editing routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientDashboardComponent } from './client-dashboard/client-dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path:'',component:SignupComponent},
   {path:'login',component:LoginComponent},
+  {path:'client_dashboard',component:ClientDashboardComponent}
 ];
 
 @NgModule({
